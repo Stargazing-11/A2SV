5dfa170d9d71626301005416f5cc37d2eae8e9ef
Stargazing-11 <samuelkifle199314@gmail.com>
2022-07-09 23:15:12 +0300

    A2SV--LeetCode-- 1438 -- Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit Edited

--- July--Week-2/ Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit--Leetcode--1438.py
+++ July--Week-2/ Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit--Leetcode--1438.py
@@ -1,32 +1,32 @@
-    class Solution:
-        def longestSubarray(self, nums: List[int], limit: int) -> int:
-            monque = [nums[0]]
-            maxN = monque[0]
-            minN = monque[0]
-            ans = 1
-            for i in range(1, len(nums)):
-                new = nums[i]
-                monque.append(new)
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        monque = [nums[0]]
+        maxN = monque[0]
+        minN = monque[0]
+        ans = 1
+        for i in range(1, len(nums)):
+            new = nums[i]
+            monque.append(new)
 
-                if new > maxN:
-                    maxN = new
-                elif new < minN:
-                    minN = new
-                if new>maxN:
-                    maxN=new
-                if new < minN:
-                    minN=new
+            if new > maxN:
+                maxN = new
+            elif new < minN:
+                minN = new
+            if new>maxN:
+                maxN=new
+            if new < minN:
+                minN=new
 
-                if maxN-minN <= limit:
-                    ans = max(ans, len(monque))
-                else:
+            if maxN-minN <= limit:
+                ans = max(ans, len(monque))
+            else:
+                monque.pop(0)
+                while max(monque)- min(monque) > limit:
+                    if len(monque) > 3000:
+                        return ans
                     monque.pop(0)
-                    while max(monque)- min(monque) > limit:
-                        if len(monque) > 3000:
-                            return ans
-                        monque.pop(0)
-                    maxN = max(monque)
-                    minN = min(monque)
-            return ans
+                maxN = max(monque)
+                minN = min(monque)
+        return ans
 
                     
