8be2204f4bfdc48bc7e251b63ad6de92c904ce23
Stargazing-11 <samuelkifle199314@gmail.com>
2022-10-07 11:21:29 +0300

    changed

--- Oct-Week1/Maximum Points You Can Obtain from Cards -- Leetcode -- 1423.py
+++ Oct-Week1/Maximum Points You Can Obtain from Cards -- Leetcode -- 1423.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        
+
         left, right = 1, len(cardPoints) - k
-        
+
         rightSum = sum(cardPoints[right:])
         answer = rightSum
 
         while right < len(cardPoints):
             rightSum = rightSum - cardPoints[right] + cardPoints[left-1]
-            answer= max(answer, rightSum)
+            answer = max(answer, rightSum)
             left += 1
             right += 1
         return answer
-            
