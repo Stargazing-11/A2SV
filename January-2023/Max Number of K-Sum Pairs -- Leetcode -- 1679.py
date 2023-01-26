ea808a5394edd3e025550808c114cb0fb41935e2
Stargazing-11 <samuelkifle199314@gmail.com>
2023-01-26 18:28:07 +0300

    A2SV -- Leetcode -- 1679

--- January-2023/Max Number of K-Sum Pairs -- Leetcode -- 1679.py
+++ January-2023/Max Number of K-Sum Pairs -- Leetcode -- 1679.py
@@ -1,16 +1,32 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
+        # hash map solution
+        def hashmapSolution():
+            hashmap = {}
+            count = 0
+            for num in nums:
+                if k - num in hashmap and hashmap[k-num] > 0:
+                    count += 1
+                    hashmap[k-num] -= 1
+                else:
+                    if num in hashmap:
+                        hashmap[num] += 1
+                    else:
+                        hashmap[num] = 1
+            return count
+        # Two Pointer approach
 
-        left, right = 0, len(nums)-1
-        count = 0
-        while left < right:
-            if nums[left] + nums[right] > k:
-                right -= 1
-            elif nums[left] + nums[right] < k:
-                left += 1
-            else:
-                count += 1
-                left += 1
-                right -= 1
-        return count
+        def twoPointerandSortingSolution():
+            nums.sort()
+            left, right = 0, len(nums)-1
+            count = 0
+            while left < right:
+                if nums[left] + nums[right] > k:
+                    right -= 1
+                elif nums[left] + nums[right] < k:
+                    left += 1
+                else:
+                    count += 1
+                    left += 1
+                    right -= 1
+            return count
