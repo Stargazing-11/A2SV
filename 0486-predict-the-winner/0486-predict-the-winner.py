97b511b7a5d133b7360350c5fe291b1120f6ea1f
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-08 16:46:32 +0300

    Time: 8011 ms (5.02%), Space: 13.9 MB (76.04%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,17 +1,14 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        player1 = 0
-        player2 = 0
         
-        def predict(left, right, player1, player2, turn):
-            if left > right:
-                return player1 >= player2 
-            if turn:
-                p1c1 = predict(left+1, right, player1 + nums[left], player2, 1-turn)
-                p1c2 = predict(left, right - 1, player1 + nums[right], player2, 1-turn)
-                return p1c1 or p1c2
-            else:
-                p2c1 = predict(left+1, right, player1, player2 + nums[left], 1-turn)
-                p2c2 = predict(left, right - 1, player1, player2 + nums[right], 1-turn)
-                return p2c1 and p2c2
-        return predict(0, len(nums)-1, player1, player2, 1)
+        def predict(l, r):
+            if l > r:
+                return 0
+            
+            choice1 = nums[l] + min(predict(l+2, r), predict(l+1, r-1))
+            
+            choice2 = nums[r] + min(predict(l+1, r-1), predict(l, r-2))
+            
+            return max(choice1, choice2)
+            
+        return 2*(predict(0, len(nums)-1)) >= sum(nums)
