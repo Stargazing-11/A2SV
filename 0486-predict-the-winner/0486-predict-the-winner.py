9785eb1899c0145247e06bf8578ca928fc36abca
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-08 16:55:12 +0300

    Time: 25 ms (99.15%), Space: 14 MB (36.86%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,13 +1,12 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         @lru_cache(None)
-        def predict(l, r):
-            if l > r:
+        def predict(left, right):
+            if left > right:
                 return 0
             
-            choice1 = nums[l] + min(predict(l+2, r), predict(l+1, r-1))
-            
-            choice2 = nums[r] + min(predict(l+1, r-1), predict(l, r-2))
+            choice1 = nums[left] + min(predict(left+2, right), predict(left+1, right-1))
+            choice2 = nums[right] + min(predict(left+1, right-1), predict(left, right-2))
             
             return max(choice1, choice2)
             
