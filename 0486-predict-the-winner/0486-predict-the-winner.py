6635e83e0845b5ebae380d26846e64cbda91e56f
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-08 16:49:17 +0300

    Time: 45 ms (45.21%), Space: 14.2 MB (23.18%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,18 +1,14 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        
-        def predict(l, r, memo):
-            state = (l, r)
-            if state in memo:
-                return memo[state]
+        @cache
+        def predict(l, r):
             if l > r:
                 return 0
             
-            choice1 = nums[l] + min(predict(l+2, r, memo), predict(l+1, r-1, memo))
+            choice1 = nums[l] + min(predict(l+2, r), predict(l+1, r-1))
             
-            choice2 = nums[r] + min(predict(l+1, r-1, memo), predict(l, r-2, memo))
+            choice2 = nums[r] + min(predict(l+1, r-1), predict(l, r-2))
             
-            memo[state] = max(choice1, choice2)
             return max(choice1, choice2)
             
-        return 2*(predict(0, len(nums)-1, {})) >= sum(nums)
+        return 2*(predict(0, len(nums)-1)) >= sum(nums)
