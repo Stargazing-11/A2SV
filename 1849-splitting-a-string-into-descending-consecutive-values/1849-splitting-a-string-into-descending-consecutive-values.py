6938159a56c595307eb5b9dc0664af1475bdaf6b
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-23 14:23:47 +0300

    Time: 39 ms (51.01%), Space: 13.9 MB (60.59%) - LeetHub

--- 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
+++ 1849-splitting-a-string-into-descending-consecutive-values/1849-splitting-a-string-into-descending-consecutive-values.py
@@ -1,21 +1,26 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-            
+        n = len(s)
         if len(s) == 1:
             return False
         
-        def backtrack(cur, s):
-            if len(s) == 1 and cur - int(s) == 1 or len(s) == 0:
+        def backtrack(cur, nxt, candidate):
+            
+            if n - nxt <= 1:
+                if n - nxt == 0:
                     return True
-            elif len(s) <= 1:
-                return False
-
+                elif cur - int(candidate[nxt]) == 1:
+                    return True
+                else:
+                    return False
+                
             newcur = 0
             ans = False
-            for i in range(len(s)):
-                newcur = (newcur*10) + int(s[i])
+            for i in range(nxt, len(candidate)):
+                newcur = (newcur*10) + int(candidate[i])
+                
                 if int(cur) - int(newcur) == 1:
-                    ans = backtrack(newcur, s[i+1:]) 
+                    ans = backtrack(newcur, i+1, candidate) 
                 elif int(newcur) - int(cur) > 1:
                     return False
                 if ans:
@@ -25,7 +30,7 @@ class Solution:
         first = 0
         for i in range(len(s) - 1):
             first = (first*10) + int(s[i])
-            ans = backtrack(first, s[i+1:])
+            ans = backtrack(first, i+1, s)
             if ans:
                 break
         return ans 
