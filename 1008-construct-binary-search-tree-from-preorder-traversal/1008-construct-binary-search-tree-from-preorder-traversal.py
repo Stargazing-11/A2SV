226c439a98779d6c6bbe7823404086efaa31fef6
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-26 14:20:43 +0300

    Time: 43 ms (27.98%), Space: 14 MB (41.44%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -6,31 +6,31 @@
 #         self.right = right
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        preorder = preorder
         root = TreeNode()
+        
+        def binarySearch(left, right, target):
+            while left <= right:
+                mid = (left + right) // 2
+                if preorder[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
         
         def construct(root, start, end, turn):
             if start == end:
                 return 
-            left, right = start + 1, end-1
+            left = binarySearch(start+1, end-1, preorder[start])
             
-            while left <= right:
-                mid = (left + right) // 2
-                
-                if preorder[mid] < preorder[start]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
             if turn == 0:
                 root.left = TreeNode(preorder[start])
-                construct(root.left, start+1, left,  0)
-                construct(root.left, left, end,  1)
-                
+                newRoot = root.left
             else:
                 root.right = TreeNode(preorder[start])
-
-                construct(root.right, start+1, left,  0)
-                construct(root.right, left, end,  1)
+                newRoot = root.right
+                
+            construct(newRoot, start+1, left,  0)
+            construct(newRoot, left, end,  1)
             
         construct(root, 0, len(preorder), 0)
         return root.left
