278f166db14c1014b8e22fc7dc325ab728b61a12
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-26 10:53:41 +0300

    Time: 75 ms (32.25%), Space: 14 MB (6.88%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,41 +10,34 @@ class Solution:
         carry = 0
         def add(head1, head2, carry):
             nonlocal totalSum
+            num = -1
+            
             if head1 and head2:
                 temp = (head1.val + head2.val + carry)
-                if temp >= 10:
-                    carry = temp // 10
-                    temp = temp % 10
-                else:
-                    carry = 0
-                totalSum += str(temp)  
-                add(head1.next, head2.next, carry)
-                
-                
+                num = 0
             elif head1 and not head2:
                 temp = (head1.val + carry)
-                if temp >= 10:
-                    carry = temp // 10
-                    temp = temp % 10
-                else:
-                    carry = 0
-                totalSum += str(temp)  
-                add(head1.next, head2, carry)
-                
+                num = 1
             elif head2 and not head1:
                 temp = (head2.val + carry)
-                if temp >= 10:
-                    carry = temp // 10
-                    temp = temp % 10
-                else:
-                    carry = 0
-                totalSum += str(temp)  
-                add(head1, head2.next, carry)
-                
-            elif not head1 and not head2:
+            else:
                 if carry > 0:
                     totalSum += str(carry)
-                return    
+                return
+            if temp >= 10:
+                carry = temp // 10
+                temp = temp % 10
+            else:
+                carry = 0
+            totalSum += str(temp)  
+            
+            if num == 0:
+                add(head1.next, head2.next, carry)
+            elif num == 1:
+                add(head1.next, head2, carry)
+            else:
+                add(head1, head2.next, carry)
+                    
         add(l1, l2, carry)
         head = ListNode()
         
