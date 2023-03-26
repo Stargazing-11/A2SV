3920acac6cd3dcaeb3de3723931e920ac1b67947
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-03-26 10:45:29 +0300

    Time: 82 ms (12.59%), Space: 14.1 MB (6.88%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,26 +5,54 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        num1, num2 = [], []
+
+        totalSum = ""
+        carry = 0
+        def add(head1, head2, carry):
+            nonlocal totalSum
+            if head1 and head2:
+                temp = (head1.val + head2.val + carry)
+                if temp >= 10:
+                    carry = temp // 10
+                    temp = temp % 10
+                else:
+                    carry = 0
+                totalSum += str(temp)  
+                add(head1.next, head2.next, carry)
+                
+                
+            elif head1 and not head2:
+                temp = (head1.val + carry)
+                if temp >= 10:
+                    carry = temp // 10
+                    temp = temp % 10
+                else:
+                    carry = 0
+                totalSum += str(temp)  
+                add(head1.next, head2, carry)
+                
+            elif head2 and not head1:
+                temp = (head2.val + carry)
+                if temp >= 10:
+                    carry = temp // 10
+                    temp = temp % 10
+                else:
+                    carry = 0
+                totalSum += str(temp)  
+                add(head1, head2.next, carry)
+                
+            elif not head1 and not head2:
+                if carry > 0:
+                    totalSum += str(carry)
+                return    
+        add(l1, l2, carry)
+        head = ListNode()
         
-        temp = l1
-        while temp:
-            num1.append(str(temp.val))
-            temp = temp.next
-        
-        temp = l2
-        while temp:
-            num2.append(str(temp.val))
-            temp = temp.next
-        num1.reverse()
-        num2.reverse()
-        
-        num3 = str(int(''.join(num1)) + int(''.join(num2)))
-        
-        ans = ListNode()
-        temp = ans
-        
-        for i in num3[::-1]:
-            temp.next = ListNode(int(i))
-            temp = temp.next
-        return ans.next
+        def build(head, pos):
+            if pos >= len(totalSum):
+                return 
+            head.next = ListNode(int(totalSum[pos]))
+            head = head.next
+            build(head, pos + 1)
+        build(head, 0)
+        return head.next
