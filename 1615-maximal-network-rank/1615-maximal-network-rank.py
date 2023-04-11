c730e5550ff5689e64e62c0fb547a1e65199f21a
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-04-11 18:11:42 +0300

    Time: 360 ms (43.66%), Space: 16.1 MB (44.40%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.py
+++ 1615-maximal-network-rank/1615-maximal-network-rank.py
@@ -1,28 +1,21 @@
 class Solution:
     def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
         graph = {}
-        
-        for road in roads:
-            if road[0] not in graph:
-                graph[road[0]] = set()
-                graph[road[0]].add(tuple(road))
-            else:
-                graph[road[0]].add(tuple(road))
+        degrees = {}
+        new_roads = set()
+        for city1, city2 in roads:
+            new_roads.add((city1, city2))
             
-            if road[1] not in graph:
-                graph[road[1]] = set()
-                graph[road[1]].add(tuple(road))
-            else:
-                graph[road[1]].add(tuple(road))
-                
-        keys = list(graph.keys())
+            degrees[city1] = degrees.get(city1, 0) + 1
+            degrees[city2] = degrees.get(city2, 0) + 1
+            
+        keys = list(degrees.keys())
         max_network = 0
         
         for i in range(len(keys)):
             for j in range(i + 1, len(keys)):
-                temp = len(graph[keys[i]]) + len(graph[keys[j]])
-                if (keys[i], keys[j]) in graph[keys[i]] or (keys[j], keys[i]) in graph[keys[i]]:
+                temp = degrees[keys[i]] + degrees[keys[j]]
+                if (keys[i], keys[j]) in new_roads or (keys[j], keys[i]) in new_roads:
                     temp -= 1
                 max_network = max(max_network, temp)
-        
         return max_network
