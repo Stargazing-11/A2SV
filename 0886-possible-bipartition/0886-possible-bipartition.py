ee80635ee3dc71e967710a4e09f386e363e97b0a
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-04-12 19:29:27 +0300

    Time: 716 ms (56.28%), Space: 21.9 MB (27.13%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,33 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        
+        graph = defaultdict(list)
+        
+        for vert, hate in dislikes:
+            graph[vert].append(hate)
+            graph[hate].append(vert)
+            
+        colors = [1 for i in range(n)]
+        
+        def dfs(vertex, color):
+            if colors[vertex-1] != 1:
+                if colors[vertex-1] != color:
+                    return False
+                return True
+            
+            colors[vertex-1] = color
+            
+            for neighbor in graph[vertex]:
+                ans = dfs(neighbor, (color + 1) * (-1))
+                if ans == False:
+                    return False
+            return True
+
+        for i in range(1, n+1):
+            if colors[i-1] == 1:
+                ans = dfs(i, 0)
+                if ans == False:
+                    return False
+        return True
+            
+            
