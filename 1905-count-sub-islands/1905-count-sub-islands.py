cfbeb9201e34425c3a247954a251da1148504480
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-04-13 22:13:31 +0300

    Time: 3651 ms (17.80%), Space: 98.9 MB (36.23%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,38 +1,31 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        visited = set()
+        count = 0
         
         def inBound(row, col):
             return 0 <= row < len(grid2) and 0 <= col < len(grid2[0])
         
-        paths = []
-        visited = set()
-        def dfs(row, col, cur):
+        def dfs(row, col, flg):
             visited.add((row, col))
-            cur.append((row, col))
+            if not grid1[row][col]:
+                flg[0] = False
+                
             for r, c in directions:
                 new_r = row + r
                 new_c = col + c
                 
                 if inBound(new_r, new_c) and (new_r, new_c) not in visited and grid2[new_r][new_c]:
-                    dfs(new_r, new_c, cur)
-        
+                    dfs(new_r, new_c, flg)
+                    
+                    
         for i in range(len(grid2)):
             for j in range(len(grid2[0])):
                 if (i, j) not in visited and grid2[i][j]:
-                    cur = []
-                    dfs(i, j, cur)
-                    paths.append(cur)
-        
-        count = 0
-        for path in paths:
-            flg = False
-            for row, col in path:
-                if not grid1[row][col]:
-                    flg = True
-                    break
-            if not flg:
-                count +=1 
-                
+                    flg = [True]
+                    dfs(i, j, flg)
+                    if flg[0]:
+                        count += 1
         return count
         
