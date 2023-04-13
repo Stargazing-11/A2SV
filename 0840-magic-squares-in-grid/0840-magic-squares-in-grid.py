ac232c07217f77f488b1d753abdb7ab2d7a03ee3
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-04-13 23:16:05 +0300

    Time: 41 ms (60.00%), Space: 13.9 MB (38.79%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -1,6 +1,7 @@
 class Solution:
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        def check_unq(r, c):
+        #check_unique_elements
+        def is_unique(r, c):
             elem = set()
             for i in range(r, r+3):
                 for j in range(c, c + 3):
@@ -9,10 +10,12 @@ class Solution:
                     elem.add(grid[i][j])
             return True
         
+        #check_bound
         def inBound(r, c):
             return 0 <= r+3 <= len(grid) and 0 <= c+3 <= len(grid[0]) 
         
-        def check_range(r, c):
+        #check_range(1, 9)
+        def is_in_range(r, c):
             for i in range(r, r+3):
                 for j in range(c, c + 3):
                     if 1 > grid[i][j] or 9 < grid[i][j]:
@@ -21,7 +24,7 @@ class Solution:
         
         
         def check_sum(r, c):
-            if not check_unq(r, c) or not check_range(r, c):
+            if not is_unique(r, c) or not is_in_range(r, c):
                 return False
             
             new = [grid[i][c:c+3].copy() for i in range(r, r+3)]
@@ -31,8 +34,8 @@ class Solution:
             col_sum = {}
             for i in range(3):
                 if sum(new[i]) != check:
-                    
                     return False
+                
                 col_sum[0] = col_sum.get(0, 0) + new[i][0]
                 col_sum[1] = col_sum.get(1, 0) + new[i][1]
                 col_sum[2] = col_sum.get(2, 0) + new[i][2]
@@ -41,15 +44,14 @@ class Solution:
             for i in range(3):
                 if col_sum[i] != check:
                     return False
-            
+            #diagonal sum 1
             if new[0][0] + new[1][1] + new[2][2] != check:
                 return False
+            # diagonal sum 2
             if new[0][2] + new[1][1] + new[2][0] != check:
                 return False
             return True
-            
-            
-        
+
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
