bba63f82b838900edf7c8d93a65cc910d0150614
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-04-13 23:13:05 +0300

    Time: 32 ms (92.73%), Space: 13.9 MB (38.79%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.py
@@ -0,0 +1,59 @@
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        def check_unq(r, c):
+            elem = set()
+            for i in range(r, r+3):
+                for j in range(c, c + 3):
+                    if grid[i][j] in elem:
+                        return False
+                    elem.add(grid[i][j])
+            return True
+        
+        def inBound(r, c):
+            return 0 <= r+3 <= len(grid) and 0 <= c+3 <= len(grid[0]) 
+        
+        def check_range(r, c):
+            for i in range(r, r+3):
+                for j in range(c, c + 3):
+                    if 1 > grid[i][j] or 9 < grid[i][j]:
+                        return False
+            return True
+        
+        
+        def check_sum(r, c):
+            if not check_unq(r, c) or not check_range(r, c):
+                return False
+            
+            new = [grid[i][c:c+3].copy() for i in range(r, r+3)]
+            check = sum(new[0])
+            
+            # check row sum   
+            col_sum = {}
+            for i in range(3):
+                if sum(new[i]) != check:
+                    
+                    return False
+                col_sum[0] = col_sum.get(0, 0) + new[i][0]
+                col_sum[1] = col_sum.get(1, 0) + new[i][1]
+                col_sum[2] = col_sum.get(2, 0) + new[i][2]
+                
+            # check col sum
+            for i in range(3):
+                if col_sum[i] != check:
+                    return False
+            
+            if new[0][0] + new[1][1] + new[2][2] != check:
+                return False
+            if new[0][2] + new[1][1] + new[2][0] != check:
+                return False
+            return True
+            
+            
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if inBound(i, j):
+                    if check_sum(i, j):
+                        count += 1
+        return count
