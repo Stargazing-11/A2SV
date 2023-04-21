0632d9b714caa5c5308467905b637758949cc40b
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-04-21 14:36:28 +0300

    Time: 129 ms (89.19%), Space: 14.3 MB (59.34%) - LeetHub

--- 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
+++ 0914-x-of-a-kind-in-a-deck-of-cards/0914-x-of-a-kind-in-a-deck-of-cards.py
@@ -1,17 +1,15 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
+        
         def gcf(a, b):
             if b == 0:
                 return a
             return gcf(b, a%b)
         
         counted = Counter(deck)
-        keys = list(counted.keys())
-        cur_gcf = counted[keys[0]]
         
-        for key in keys[1:]:
+        cur_gcf = counted[counted[0]]
+        for key in counted:
             cur_gcf = gcf(cur_gcf, counted[key])
         
-        if cur_gcf > 1:
-            return True
-        return False
+        return cur_gcf > 1
