b256732d44c4fe698cd85ac8447245b542a4a7ae
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-09-18 12:06:56 +0300

    Update 0650-2-keys-keyboard.py

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,27 +1,22 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        def prime_sieve(n):
-            is_prime = [True for _ in range(n + 1)]
-            is_prime[0] = is_prime[1] = False
-
-            i = 2
-            while i * i <= n:
-                if is_prime[i]:
-                    j = i * i
-                    while j <= n:
-                        is_prime[j] = False
-                        j += i
-                i += 1
-
-            return [i for i in range(n+1) if is_prime[i]==True]
         
-        primes = prime_sieve(n)
-        oprs = 0
-        i = 0
-        while n > 1:
-            if n % primes[i] == 0:
-                n = n // primes[i]
-                oprs += primes[i]
-            else:
-                i += 1
-        return oprs
+        @cache
+        def dp(copied, cur):
+            if cur > n:
+                return float('inf')
+            
+            if cur == n:
+                return 0
+
+            copy, paste = float('inf'), float('inf')
+            
+            if copied:
+                copy = dp(copied, cur + copied) + 1
+            
+            if cur > copied:
+                paste = dp(cur, cur) + 1
+            
+            return min(copy, paste)
+                       
+        return dp(0, 1)
