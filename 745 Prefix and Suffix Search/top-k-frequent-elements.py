c2da71679cf8d1e7eaacd8dee2c8eccd2cbb6215
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-10-03 21:29:48 +0300

    Added solution for Top K Frequent Elements

---
+++ 745 Prefix and Suffix Search/top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        z = Counter(nums)
+        y = sorted(z, key=z.get, reverse=True)
+        c = 0
+        out = []
+        while(c < k):
+            out.append(y[c])
+            c += 1
+        return out
+            
