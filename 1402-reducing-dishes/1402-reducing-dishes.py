84e9fb1cb6f1726d1437e44a53bb8d11b4e707fd
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-10-10 17:46:06 +0300

    Time: 312 ms (23.76%), Space: 22 MB (23.55%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -4,16 +4,23 @@ class Solution:
         cache = {}
         satisfaction.sort()
         
-        def dp(i, cur_time):
+#         def dp(i, cur_time):
             
-            if i >= len(satisfaction):
-                return 0
+#             if i >= len(satisfaction):
+#                 return 0
             
-            if (i, cur_time) in cache:
-                return cache[(i, cur_time)]
+#             if (i, cur_time) in cache:
+#                 return cache[(i, cur_time)]
             
-            cache[(i, cur_time)] = max(cur_time * satisfaction[i] + dp(i+1, cur_time +1), dp(i+1, cur_time))
+#             cache[(i, cur_time)] = max(cur_time * satisfaction[i] + dp(i+1, cur_time +1), dp(i+1, cur_time))
             
-            return cache[(i, cur_time)]
+#             return cache[(i, cur_time)]
         
-        return dp(0, 1)
+        # return dp(0, 1)
+        n= len(satisfaction)
+        dp = [[0] * (n +1) for _ in range(n + 1)] 
+
+        for i in range(n-1, -1, -1):
+            for cur_time in range(i+1):
+                dp[cur_time][i] = max(dp[cur_time][i+1], ((cur_time + 1) * satisfaction[i]) + dp[cur_time + 1][i+1])
+        return dp[0][0]
