bbb8950e0253767e593f47a67e0e771cb232ac03
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-10-10 18:50:08 +0300

    Time: 5986 ms (5.00%), Space: 16.2 MB (99.61%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -1,40 +1,42 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
+        max_score = [0]
+        def calculateGcd(cur):
+            gcds = []
+            for a, b in cur:
+                gcds.append(math.gcd(a,b))
+            gcds.sort()
+            cur_gcd = 0
+            for i in range(n//2):
+                cur_gcd += (i+1) * gcds[i]
+            max_score[0] = max(max_score[0], cur_gcd)
         
         
         n = len(nums)
-        max_score = [0]
+        
         def backtrack(cur, visited):
             if len(cur) == n // 2:
-                gcds = []
-                for a, b in cur:
-                    gcds.append(math.gcd(a,b))
-                gcds.sort()
-                
-                cur_gcd = 0
-                for i in range(n//2):
-                    cur_gcd += (i+1) * gcds[i]
-                max_score[0] = max(max_score[0], cur_gcd)
+                calculateGcd(cur)
                 return 
-
-            prev = -1
             
+            start = -1
             for i, num in enumerate(nums):
                 if i not in visited:
-                    prev = i 
+                    start = i 
                     break
                     
-            for j in range(prev+1, n):
+            for j in range(start+1, n):
                 if j not in visited:
                     visited.add(j)
-                    visited.add(prev)
-                    cur.append((nums[prev], nums[j]))
+                    visited.add(start)
+                    cur.append((nums[start], nums[j]))
                     backtrack(cur, visited)
                     if cur:
                         a, b = cur.pop()
                         visited.remove(j)
-                        visited.remove(prev)
+                        visited.remove(start)
             return 
+        
         visited = set()
         backtrack([], visited)
         
