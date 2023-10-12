431d37c2353743c354cd1b446a9239a673ab822e
Samuel Kifle <79416997+Stargazing-11@users.noreply.github.com>
2023-10-12 15:03:23 +0300

    Added solution for Count Number of Teams

---
+++ 1395. Count Number of Teams/count-number-of-teams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+        n = len(rating)
+        greater = [0 for _ in range(n)]
+        smaller = [0 for _ in range(n)]
+        count = 0
+        
+        for i in range(n):
+            for j in range(i):
+                if rating[j] < rating[i]:
+                    count += greater[j]
+                    greater[i] += 1
+                else:
+                    count += smaller[j]
+                    smaller[i] += 1
+        return count
+                    
